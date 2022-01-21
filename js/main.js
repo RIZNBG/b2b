977a9216003d1b85498cf75ecd0bb5cbc8a4ecba
Hristo Atanasov <hristoa2710@gmail.com>
2022-01-21 12:57:17 +0200

    ADD: Products navigation

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 $("document").ready(function(){
-    $(".tab-slider--body").hide();
-    $(".tab-slider--body:first").show();
+    $(".tab-slider--body, .slider-prd").hide();
+    $(".tab-slider--body:first, .slider-prd:first").show();
 });
 
 $(".tab-slider--nav li").click(function() {
-    $(".tab-slider--body").hide();
-    var activeTab = $(this).attr("rel");
-    $("#"+activeTab).fadeIn();
-        if($(this).attr("rel") == "tab2"){
-            $('.tab-slider--tabs').addClass('slide');
+    var self = $(this);
+    var currentSwitch = self.closest(".switch-slider");
+    currentSwitch.find(".tab-slider--body").hide();
+    var activeTab = self.attr("rel");
+    currentSwitch.find("#"+activeTab).fadeIn();
+        if(self.attr("rel") == "tab2"){
+            currentSwitch.find('.tab-slider--tabs').addClass('slide');
         }else{
-            $('.tab-slider--tabs').removeClass('slide');
+            currentSwitch.find('.tab-slider--tabs').removeClass('slide');
         }
-    $(".tab-slider--nav li").removeClass("active");
-    $(this).addClass("active");
+    currentSwitch.find(".tab-slider--nav li").removeClass("active");
+    self.addClass("active");
 });
